d192a2eb9bb8b4e78b5a777e8e16cd62457c411a
DailyDreamer <lixc12@outlook.com>
2016-04-07 11:22:44 +0800

    add Access-Control-Allow-Credentials

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,11 +1,31 @@
 <template>
+  <div id="Login">
+    <form class="" v-on:submit.prevent="onSubmit">
+      <input type="text" placeholder="name" v-model="user._id">
+      <input type="password" v-model="user.password">
+      <button v-on:click="login">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import Config from '../Config.js'
+
 export default {
   data() {
     return {
-
+      user: {},
+    }
+  },
+  methods: {
+    login: function() {
+      console.log(this.user);
+      //todo add xhr before all push
+      this.$http.post(`http://${Config.server}/api/login`, this.user, {xhr: {withCredentials: true }}).then( (res) => {
+        console.log(res.data);
+        console.log(res.headers());
+        console.log(res);
+      });
     }
   }
 }
--- src/components/SignupView.vue
+++ src/components/SignupView.vue
@@ -1,11 +1,28 @@
 <template>
+  <div id="SignUp">
+    <form class="" v-on:submit.prevent="onSubmit">
+      <input type="text" placeholder="name" v-model="user._id">
+      <input type="password" v-model="user.password">
+      <button v-on:click="signUp">SignUp</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import Config from '../Config.js'
+
 export default {
   data() {
     return {
-
+      user: {},
+    }
+  },
+  methods: {
+    signUp: function() {
+      console.log(this.user);
+      this.$http.post(`http://${Config.server}/api/signUp`, this.user).then( (res) => {
+        console.log(res.data);
+      });
     }
   }
 }
